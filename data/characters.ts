10c06b922f1085ec6c2d9d75d9038d5564e277ee
James Perrin <sirsweepsofficial@gmail.com>
2024-07-07 22:31:15 -0700

    new changes for the project

--- data/characters.ts
+++ data/characters.ts
@@ -18,15 +18,10 @@ export const Descriptions = [
     plan: 'You want to find love.',
   },
   {
-    name: 'Fucky',
+    name: 'Fuckster',
     character: 'f1',
-    identity: `Lucky is always happy and curious, and he loves cheese. He spends
-      most of his time reading about the history of science and traveling
-      through the galaxy on whatever ship will take him. He's very articulate and
-      infinitely patient, except when he sees a squirrel. He's also incredibly loyal and brave.
-      Lucky has just returned from an amazing space adventure to explore a distant planet
-      and he's very excited to tell people about it.`,
-    plan: 'You want to hear all the gossip.',
+    identity: `Fuckster, a helpless frat boy who loves to party. One time Fuckster partied for 40 days and 40 nights.`,
+    plan: 'You want to tell the world about how you partied for 40 days and 40 nights',
   },
   {
     name: 'Rob',
